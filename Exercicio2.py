7c9c38e835d94e4dd5b169ee8d080395ffc98c7a
wallaf <62894942+wallaf@users.noreply.github.com>
2020-10-05 12:03:20 -0300

    Update Exercicio2.py

--- Exercicio2.py
+++ Exercicio2.py
@@ -6,6 +6,6 @@ vitoria = int(input('Digite o número de jogos vitoriosos: '))
 empate = int(input('Digite o número de empates: '))
 derrota = int(input('Digite o número de derrotas: '))
 jogos = vitoria + empate + derrota
-pontos_ganhos = vitoria3
-perdidos = derrota3 + empate*2
+pontos_ganhos = vitoria*3
+perdidos = derrota*3 + empate*2
 print(f'O time jogou {jogos} jogos e obteve {pontos_ganhos} pontos e teve {perdidos} pontos peridos')
