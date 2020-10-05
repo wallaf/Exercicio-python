b70e6bf52eb8b3e54d3d12ea9989060a741d9792
wallaf <62894942+wallaf@users.noreply.github.com>
2020-10-05 12:22:03 -0300

    Update Exercicio5.py

--- Exercicio5.py
+++ Exercicio5.py
@@ -4,16 +4,16 @@
 
 menor160 = maior180 = entre160e180 = 0
 
-for calc in range(10):
+for calc in range(3):
     altura = float(input(f'Digite a altura da {calc + 1}º pessoa: '))
 
-    if altura < 1.60:
+    if altura < 1.6:
         menor160 += 1
-    elif altura > 1.80:
+    elif altura > 1.8:
         maior180 += 1
-    elif 1.60 < altura < 1.80:
+    elif 1.6 <= altura <= 1.80:
         entre160e180 += 1
 
-print(f'Menores ou iguais a 1.60 foram {menor160}')
-print(f'Maiores ou iguais a 1.80 foram {maior180}')
+print(f'Menores a 1.60 foram {menor160}')
+print(f'Maiores a 1.80 foram {maior180}')
 print(f'Entre 1.60 e 1.80 foram {entre160e180}')
